fee01853cc200f9b3d437165ecf1fc6265c5c117
William Vallet <valletw@gmail.com>
2019-12-02 19:13:39 +0100

    day2 part2

--- day2.py
+++ day2.py
@@ -17,33 +17,50 @@ class Day2:
         # Convert input data to integer array.
         for i in list:
             data.append(int(i))
-        # Set 1202 program alarm state.
-        data[1] = 12
-        data[2] = 2
+        # Day 2.1: 1202 progrma alarm.
+        day2_1 = self.intcode_execute(data, 12, 2)
+        print(f"Day2.1: {day2_1}")
+        # Day 2.2: Search parameter for output 19690720.
+        for noun in range(0, 99):
+            for verb in range(0,99):
+                if self.intcode_execute(data, noun, verb) == 19690720:
+                    print(f"Day2.2: {100 * noun + verb}")
+
+    def intcode_execute(self, memory, noun, verb):
+        # Copy memory for running program.
+        runtime = memory.copy()
+        # Set noun and verb parameter.
+        runtime[1] = noun
+        runtime[2] = verb
         # Execute program.
         i = 0
-        while i < len(data):
-            opcode = data[i]
+        while i < len(runtime):
+            opcode = runtime[i]
+            # Add instruction.
             if opcode == 1:
                 # Get arguments.
-                arg1 = data[i+1]
-                arg2 = data[i+2]
-                out = data[i+3]
+                arg1 = runtime[i+1]
+                arg2 = runtime[i+2]
+                out = runtime[i+3]
                 # Move to next opcode.
                 i += 4
                 # Add arguments, and set value to out position.
-                data[out] = data[arg1] + data[arg2]
+                runtime[out] = runtime[arg1] + runtime[arg2]
+            # Multiply instruction.
             elif opcode == 2:
                 # Get arguments.
-                arg1 = data[i+1]
-                arg2 = data[i+2]
-                out = data[i+3]
+                arg1 = runtime[i+1]
+                arg2 = runtime[i+2]
+                out = runtime[i+3]
                 # Move to next opcode.
                 i += 4
                 # Multiply arguments, and set value to out position.
-                data[out] = data[arg1] * data[arg2]
+                runtime[out] = runtime[arg1] * runtime[arg2]
+            # Halt instruction.
             elif opcode == 99:
-                exit(f"data[0]: {data[0]}")
+                break
+        # Return program output.
+        return runtime[0]
 
 
 def parse_arguments():
