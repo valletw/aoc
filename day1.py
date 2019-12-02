cb13bc665baaeb2f57c58ac64d989867848dcd6f
William Vallet <valletw@gmail.com>
2019-12-02 18:16:34 +0100

    day1 part2

--- day1.py
+++ day1.py
@@ -8,6 +8,7 @@ class Day1:
     """ Fuel required to launch a given module is based on its mass.
     Specifically, to find the fuel required for a module, take its mass, divide
     by three, round down, and subtract 2. """
+    """ Part 2: add fuel requirement for fuel mass until no more required. """
     def __init__(self, input_file):
         self.process(input_file)
 
@@ -15,7 +16,14 @@ class Day1:
         total_fuel = 0
         with open(input_file, "r") as input:
             for mass in input:
-                total_fuel += self.get_fuel(int(mass))
+                fuel = self.get_fuel(int(mass))
+                total_fuel += fuel
+                # Get amont of fuel for fuel required mass.
+                while fuel > 0:
+                    fuel = self.get_fuel(fuel)
+                    # If negative fuel, no mass added.
+                    if fuel > 0:
+                        total_fuel += fuel
         print (f"Total fuel requirement: {total_fuel}")
 
     def get_fuel(self, mass):
