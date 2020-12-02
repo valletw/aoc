b1c30eac241fc2052ded05f86ea90d9f75ff8952
William Vallet <valletw@gmail.com>
2020-12-02 13:31:24 +0100

    2020: day 2 - part 2

--- 2020/day02.py
+++ 2020/day02.py
@@ -17,12 +17,16 @@ class Day2:
                     int(group[0].split("-")[1]),
                     group[1].split(":")[0],
                     group[2]))
-        match_count = 0
+        match_count_1 = 0
+        match_count_2 = 0
         for data in data_in:
             n_min, n_max, char, pw = data
             if n_min <= pw.count(char) and pw.count(char) <= n_max:
-                match_count += 1
-        print(f"Part 1: {match_count}")
+                match_count_1 += 1
+            if (pw[n_min - 1] == char) + (pw[n_max - 1] == char) == 1:
+                match_count_2 += 1
+        print(f"Part 1: {match_count_1}")
+        print(f"Part 2: {match_count_2}")
 
 
 def parse_arguments():
