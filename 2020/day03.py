8099c6be3817b5ab029a6b697266de99cbdb2d39
William Vallet <valletw@gmail.com>
2020-12-03 19:58:42 +0100

    2020: day 3 - part 2

--- 2020/day03.py
+++ 2020/day03.py
@@ -4,8 +4,9 @@ import argparse
 
 class Day3:
     """ Day 3: Toboggan Trajectory """
-    _MOVE_X = 3
-    _MOVE_Y = 1
+    _MOVE = [
+        (3, 1), (1, 1), (5, 1), (7, 1), (1, 2)
+    ]
 
     def __init__(self, input_file):
         self._trees = set()
@@ -21,14 +22,23 @@ class Day3:
                         self._trees.add((char_idx, line_idx))
                     self._x_max = char_idx + 1
                 self._y_max = line_idx + 1
-        x = 0
-        y = 0
-        nb_tree_crossed = 0
-        while y < self._y_max:
-            x += self._MOVE_X
-            y += self._MOVE_Y
-            nb_tree_crossed += self.is_tree(x, y)
-        print(f"Part 1: {nb_tree_crossed}")
+        part1_done = False
+        nb_tree_crossed_1 = 0
+        nb_tree_crossed_2 = 1
+        for mx, my in self._MOVE:
+            x = 0
+            y = 0
+            nb_tree_crossed = 0
+            while y < self._y_max:
+                x += mx
+                y += my
+                nb_tree_crossed += self.is_tree(x, y)
+            if not part1_done:
+                part1_done = True
+                nb_tree_crossed_1 = nb_tree_crossed
+            nb_tree_crossed_2 *= nb_tree_crossed
+        print(f"Part 1: {nb_tree_crossed_1}")
+        print(f"Part 2: {nb_tree_crossed_2}")
 
     def is_tree(self, x, y):
         xn = x % self._x_max
