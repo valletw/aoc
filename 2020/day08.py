0d6f80cc1a224390b281eb36419a021175e201d5
William Vallet <valletw@gmail.com>
2020-12-08 19:48:30 +0100

    2020: day 8 - part 2

--- 2020/day08.py
+++ 2020/day08.py
@@ -7,14 +7,21 @@ class Program:
     def __init__(self, prog: list, acc: int = 0):
         self._acc = acc
         self._prog = prog
+        self._end = False
 
     def get_acc(self) -> int:
         return self._acc
+
+    def has_finish(self) -> bool:
+        return self._end
 
     def process(self):
         executed = [False for _ in range(len(self._prog))]
         pc = 0
         while True:
+            if pc == len(self._prog):
+                self._end = True
+                break
             if executed[pc]:
                 break
             executed[pc] = True
@@ -43,7 +50,30 @@ class Day8:
                 self._instructions.append(line.rstrip())
         prog = Program(self._instructions)
         prog.process()
-        print(f"Part 1: {prog.get_acc()}")
+        acc_1 = prog.get_acc()
+        acc_2 = 0
+        mod_id = 0
+        while True:
+            mod_inst = self._instructions.copy()
+            for idx, inst in enumerate(mod_inst[mod_id:]):
+                op, arg = inst.split(" ")
+                if op == "nop":
+                    mod_inst[mod_id + idx] = "jmp " + arg
+                    mod_id += idx + 1
+                    break
+                elif op == "jmp":
+                    mod_inst[mod_id + idx] = "nop " + arg
+                    mod_id += idx + 1
+                    break
+            prog = Program(mod_inst)
+            prog.process()
+            if prog.has_finish():
+                acc_2 = prog.get_acc()
+                break
+            if mod_id == len(self._instructions) - 1:
+                break
+        print(f"Part 1: {acc_1}")
+        print(f"Part 2: {acc_2}")
 
 
 def parse_arguments():
