47b6d192ab3cc383be1255130fc2f53213e06ab4
William Vallet <valletw@gmail.com>
2020-12-13 19:43:33 +0100

    2020: day 13 - part 1

---
+++ 2020/day13.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+import argparse
+import math
+
+
+class Day13:
+    """ Day 13: Shuttle Search """
+
+    def __init__(self, input_file):
+        self._time_departure = 0
+        self._bus_line = list()
+        self.process(input_file)
+
+    def process(self, input_file):
+        data = list()
+        with open(input_file, "r") as input:
+            for line in input:
+                data.append(line.rstrip())
+        self._time_departure = int(data[0])
+        for bus in data[1].split(","):
+            if bus != "x":
+                self._bus_line.append(int(bus))
+        next_departure = set()
+        for bus in self._bus_line:
+            it = math.ceil(self._time_departure / bus)
+            next_departure.add((it * bus, bus))
+        next_departure = sorted(next_departure)
+        time_wait = next_departure[0][0] - self._time_departure
+        print(f"Part 1: {time_wait * next_departure[0][1]}")
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input', help="File containing day input data")
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    Day13(args.input)
