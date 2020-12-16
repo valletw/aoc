48d09f6c6eb6d7f6e6f22ec9bc50eaf374dced37
William Vallet <valletw@gmail.com>
2020-12-16 10:39:29 +0100

    2020: day 16 - part 1

---
+++ 2020/day16.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+import argparse
+
+
+class Day16:
+    """ Day 16: Ticket Translation """
+
+    def __init__(self, input_file):
+        self._ticket = list()
+        self._nearby = list()
+        self._rules = dict()
+        self.process(input_file)
+
+    def process(self, input_file):
+        with open(input_file, "r") as input:
+            nearby = False
+            ticket = False
+            for line in input:
+                line = line.rstrip()
+                if line == "your ticket:":
+                    ticket = True
+                elif line == "nearby tickets:":
+                    nearby = True
+                elif line == "":
+                    pass
+                else:
+                    if nearby:
+                        self._nearby.append([int(f) for f in line.split(",")])
+                    elif ticket:
+                        self._ticket = [int(f) for f in line.split(",")]
+                    else:
+                        rule, ranges = line.split(": ")
+                        self._rules[rule] = ranges.split(" or ")
+        error_rate = 0
+        for ticket in self._nearby:
+            for field in ticket:
+                check_success = 0
+                for _, (_, limits) in enumerate(self._rules.items()):
+                    for limit in limits:
+                        l_min, l_max = [int(l) for l in limit.split("-")]
+                        if l_min <= field and field <= l_max:
+                            check_success += 1
+                if check_success == 0:
+                    error_rate += field
+        print(f"Part 1: {error_rate}")
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input', help="File containing day input data")
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    Day16(args.input)
