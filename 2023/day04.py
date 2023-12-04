deba99088a3debe11e2ab4175c7cc8026eccf5b0
William Vallet <william.vallet@dark-space.co>
2023-12-04 10:46:10 +0100

    2023: day 4 - part 2

--- 2023/day04.py
+++ 2023/day04.py
@@ -19,16 +19,37 @@ def parse_card(puzzle_in: List[str]) -> List[Card]:
     return cards
 
 
-def compute_points(cards: List[Card]) -> List[int]:
-    points: List[int] = []
+def count_match(cards: List[Card]) -> List[int]:
+    matches: List[int] = []
     for winning, numbers in cards:
         # Count matching number between the two lists.
-        counts = sum(i in numbers for i in winning)
-        # Compute points.
-        points.append(pow(2, counts - 1) if counts != 0 else 0)
+        matches.append(sum(i in numbers for i in winning))
+    return matches
+
+
+def compute_points(matches: List[int]) -> List[int]:
+    points: List[int] = []
+    for match in matches:
+        points.append(pow(2, match - 1) if match != 0 else 0)
     return points
+
+
+def count_copy_cards(matches: List[int]) -> List[int]:
+    count = len(matches)
+    # At least, one card of each.
+    copies: List[int] = [1] * count
+    for i in range(0, count):
+        # Win one card for each card owned.
+        factor = copies[i]
+        for j in range(1, matches[i] + 1):
+            if i + j < count:
+                copies[i + j] += factor
+    return copies
 
 
 def process(puzzle_in: List[str]):
     cards = parse_card(puzzle_in)
-    print(f"Part 1: {sum(compute_points(cards))}")
+    matches = count_match(cards)
+    print(f"Part 1: {sum(compute_points(matches))}")
+    copies = count_copy_cards(matches)
+    print(f"Part 2: {sum(copies)}")
